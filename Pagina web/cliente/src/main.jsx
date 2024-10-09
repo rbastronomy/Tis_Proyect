b886dc6357e41a0b49c39deb0849d0fa66d7bdfe
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-10-09 15:25:37 -0300

    add react and vite to client

---
+++ Pagina web/cliente/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
