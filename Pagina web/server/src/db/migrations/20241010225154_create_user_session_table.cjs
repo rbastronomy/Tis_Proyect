7754540941791658c4a732194835a62b09d24e01
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-10-10 22:49:24 -0300

    migraciones para lucia auth

---
+++ Pagina web/server/src/db/migrations/20241010225154_create_user_session_table.cjs
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('user_session', function(table) {
+        table.increments('id').primary(); // Primary key
+        table.integer('user_id').unsigned().notNullable(); // Foreign key to the users table
+        table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE'); // Add reference to users table
+        table.string('token').notNullable(); // This should be a string
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('expires_at').nullable();
+    });
+};
+
+exports.down = function(knex) {
+    
+    return knex.schema.dropTable('user_session');
+};
