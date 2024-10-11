8dc22f7dda5334076d878e50b06880bda46bc8dd
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-10-10 22:45:54 -0300

    configuracion knex

---
+++ Pagina web/server/knexfile.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import process from 'process'; // Add this line to make process available
+
+dotenv.config();
+
+export default {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      getNewMigrationName: (migrationName) => {
+        return `${+new Date()}-${migrationName}.js`;
+      },
+      directory: './src/db/migrations',
+      extension: 'cjs',
+    },
+    seeds: {
+      directory: './src/db/seeds'
+    }
+  },
+};
