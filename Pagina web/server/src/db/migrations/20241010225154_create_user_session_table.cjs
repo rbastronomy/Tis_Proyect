81ad2c23bb63665cfe158ac3fdfcdc944da30402
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-10-14 18:51:13 -0300

    actualizacion de migraciones

--- Pagina web/server/src/db/migrations/20241010225154_create_user_session_table.cjs
+++ Pagina web/server/src/db/migrations/20241010225154_create_user_session_table.cjs
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
     return knex.schema.createTable('user_session', function(table) {
-        table.increments('id').primary(); // Primary key
+        table.text('id').primary(); // Primary key
         table.integer('user_id').unsigned().notNullable(); // Foreign key to the users table
         table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE'); // Add reference to users table
-        table.string('token').notNullable(); // This should be a string
+        //table.string('token').notNullable(); // This should be a string
         table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('expires_at').nullable();
+        table.timestamp('expires_at').notNullable();
     });
 };
 
