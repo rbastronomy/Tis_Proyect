3805091b5bef6b5076e4744dc5dbc7579f0701e8
MetallisVH <trabajosdelaunap@gmail.com>
2024-11-13 01:07:08 -0300

    arreglo en ruta

--- Pagina web/client/src/routes/login.lazy.jsx
+++ Pagina web/client/src/routes/login.lazy.jsx
@@ -16,7 +16,7 @@ function Login() {
 
   const onSubmit = async (data) => {
     try {
-        const response = await fetch('http://127.0.0.1:3000/api/login', {
+        const response = await fetch('api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
