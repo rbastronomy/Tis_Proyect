59e5d9492b18875393ad5709ad114e2dffd47b5a
JacqueRobles <valeria.robles.tesillo@gmail.com>
2024-12-05 19:35:17 -0300

    Cambio de relación de historial a reserva -Una reserva puede tener muchos entradas de historial

--- Pagina web/server/src/db/migrations/20241123010428_create_reserva_table.cjs
+++ Pagina web/server/src/db/migrations/20241123010428_create_reserva_table.cjs
@@ -1,20 +1,13 @@
 exports.up = function(knex) {
     return knex.schema.createTable('reserva', function(table) {
         table.increments('codigoreserva').primary();
-        
-        // Foreign key to historial
-        table.integer('idhistorial')
-            .references('idhistorial')
-            .inTable('historial')
-            .onDelete('RESTRICT')
-            .onUpdate('CASCADE');
 
         table.string('origenv', 256);
         table.string('destinov', 256);
         table.timestamp('freserva');
         table.timestamp('frealizado');
         table.string('tipo', 256);
-        table.string('observacion', 256);
+        table.text('observacion', 256);
         table.string('estados', 256);
         table.timestamp('deletedatre');
 
--- Pagina web/server/src/db/migrations/20241123010554_create_historial_table.cjs
+++ Pagina web/server/src/db/migrations/20241123010554_create_historial_table.cjs
@@ -2,8 +2,15 @@ exports.up = function(knex) {
     return knex.schema.createTable('historial', function(table) {
         table.increments('idhistorial').primary();
         table.string('estadoh', 256).notNullable();
+        table.text('observacion', 256).notNullable();
         table.timestamp('fcambio').defaultTo(knex.fn.now());
-
+        //fk to reserva
+        table.integer('codigoreserva')
+            .unsigned()
+            .references('codigoreserva')
+            .inTable('reserva')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
         table.timestamps(true, true);
     });
 };
